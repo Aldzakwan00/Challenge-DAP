bde656ac167071faa2c0d0c906b294d7013d2b80
Afif Al Dzakwan <133790227+Aldzakwan00@users.noreply.github.com>
2023-10-30 13:52:30 +0700

    Update script-login.js

--- script-login.js
+++ script-login.js
@@ -7,21 +7,36 @@ document.addEventListener('DOMContentLoaded', function () {
   eyeIcon.addEventListener('click', () => {
     if (passwordInput.type === 'password') {
       passwordInput.type = 'text';
-      eyeIcon.src = 'outlineeyeon.png';
+      eyeIcon.src = 'outlineeyeon.png'; 
     } else {
       passwordInput.type = 'password';
-      eyeIcon.src = 'outlineeyeoff.png';
+      eyeIcon.src = 'outlineeyeoff.png'; 
     }
   });
 
+  let rememberedUsername = null;
+  
   loginButton.addEventListener('click', () => {
     const username = usernameInput.value;
     const password = passwordInput.value;
 
-    if (username === 'arthurmorgan' && password === '********') {
-      alert('Anda berhasil login.');
+    if (username && password) {
+      const rememberMeCheckbox = document.getElementById('ingat-saya-checkbox');
+      if (rememberMeCheckbox.checked) {
+        localStorage.setItem('rememberedUsername', username);
+        rememberedUsername = username;
+      } else {
+        localStorage.removeItem('rememberedUsername');
+        rememberedUsername = null;
+      }
+      alert('Anda Berhasil Login');
     } else {
       alert('Login gagal. Pastikan Anda mengisi username dan password dengan benar.');
     }
   });
+
+  rememberedUsername = localStorage.getItem('rememberedUsername');
+  if (rememberedUsername) {
+    usernameInput.value = rememberedUsername;
+  }
 });
